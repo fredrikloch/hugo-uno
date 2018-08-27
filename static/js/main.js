b6742eacb77cf2ce57a3f92dd3092e079c29e573
Marshall L'Amour <marshall.lamour@thriambus.com>
2018-08-27 15:45:46 -0500

    Fixing overflow issue
    Implementing the fix from @tayfuncetin, referenced here: https://github.com/joshgerdes/jekyll-uno/issues/42#issuecomment-263251559
    
    Adapting the original Jekyll fix to the Hugo version of the theme.

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $('body').removeClass('no-js');
 
   $('a.blog-button').click(function() {
+    	$('.content-wrapper__inner').css('display', 'block');
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
@@ -16,10 +17,12 @@ $(document).ready(function() {
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+    	$('.content-wrapper__inner').css('display', 'block')
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+    	$('.content-wrapper__inner').css('display', 'block')
   }
 
   $('.btn-mobile-menu').click(function() {
